2e8bfffcd6394f7c880c240bc99843e306de8e82
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-06-04 16:46:40 +0300

    Improved view on the devise and post view folders

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,29 +2,16 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_member!, except: [:index, :show]
 
-  # GET /posts
-  # GET /posts.json
   def index
     @posts = Post.all
     @post = Post.new
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
-  def show
-  end
-
-  # GET /posts/new
   def new
    @post = current_member.posts.build
   end
 
-  # GET /posts/1/edit
-  def edit
-  end
-
-  # POST /posts
-  # POST /posts.json
+ 
   def create
     @post = current_member.posts.build(post_params)
 
@@ -39,22 +26,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
-  def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post.destroy
     respond_to do |format|
@@ -64,12 +35,11 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    
     def set_post
       @post = Post.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def post_params
       params.require(:post).permit(:body)
     end
