384471c0df8faf188a562036e13c54e5e841e0e7
OdongAlican <sandieo.2020@gmail.com>
2020-06-04 13:05:10 +0300

    adds helper methods and the navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_085617) do
+ActiveRecord::Schema.define(version: 2020_06_04_093707) do
+
+  create_table "members", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_members_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "body"
